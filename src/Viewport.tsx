a9d82e1e7193ecf3d7f3f10e3b2bbbe486608ee3
Simon Oehrl <simon.oehrl@rwth-aachen.de>
2022-09-20 14:56:53 +0200

    feat: add controls back when not in vr

--- src/Viewport.tsx
+++ src/Viewport.tsx
@@ -1,27 +1,37 @@
 import { Canvas } from '@react-three/fiber';
-import { Controllers, Hands, VRButton, XR } from '@react-three/xr';
+import { Controllers, Hands, RayGrab, VRButton, XR } from '@react-three/xr';
 import Crawler from './Crawler';
 import SkyBox from './SkyBox';
 import Water from './Water';
 import OrbitControls from './OrbitControls';
-// import { Controllers, Hands, XR } from '@react-three/xr';
 import { useConnection } from './RosbridgeConnections';
+import Show from './Show';
+import { useState } from 'react';
 
 const Viewport = () => {
   const ros = useConnection('New Connection');
+  const [isInXr, setIsInXr] = useState(false);
 
   return (
     <>
       <VRButton />
       <Canvas tabIndex={0}>
-        <XR>
+        <Show when={isInXr} fallback={<OrbitControls />}>
+          <></>
+        </Show>
+        <XR
+          onSessionStart={() => setIsInXr(true)}
+          onSessionEnd={() => setIsInXr(false)}
+        >
           <Controllers />
           <Hands />
           <SkyBox baseURL="skyboxes/clouds" />
           <Water width={1000} height={1000} waterNormalsTexture="waternormals.jpg" />
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <Crawler ros={ros} />
+          <RayGrab>
+            <Crawler ros={ros} />
+          </RayGrab>
         </XR>
       </Canvas>
     </>
